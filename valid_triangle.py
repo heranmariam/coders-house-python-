7ddb3ac5d5126d6414473e79cc6a66474666fafe
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-10-29 17:59:39 +0530

    Create valid_triangle.py

---
+++ valid_triangle.py
@@ -0,0 +1,8 @@
+a = int(input("enter the 1st angle"))
+b = int(input("enter the 2nd angle"))
+c = int(input("enter the 3rd angle"))
+total=a+b+c
+if (total==180):
+    print("is a Valid Triangle")
+else:
+    print("is not valid Triangle")
