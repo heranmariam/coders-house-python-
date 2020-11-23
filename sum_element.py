a6e38c3b6cb3ab84a411c184a6b6987aaa376cad
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-11-23 13:22:25 +0530

    Create sum_element.py

---
+++ sum_element.py
@@ -0,0 +1,10 @@
+n=int(input("enter the size of list"))
+i=0
+y=[]
+sum1=0
+while i<n:
+    k=int(input("enter integer"))
+    i+=1
+    y.append(k)
+    sum1+=k
+print("the list is",y,"\nand the sum is",sum1)
