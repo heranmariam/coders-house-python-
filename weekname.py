3d6e8c5bbdbfbbc0f35dacc196de372aceb99c8b
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-11-23 13:07:32 +0530

    Create weekname.py

---
+++ weekname.py
@@ -0,0 +1,15 @@
+week=int(input("enter the week"))
+if week==1:
+    print("sunday")
+elif week==2:
+    print("monday")
+elif(week==3):
+    print("Wednesday")
+elif(week==4):
+    print("Thursday")
+elif(week==5):
+    print("Friday")
+elif(week==6):
+    print("Saturday")
+else:
+    print("enter th week")
