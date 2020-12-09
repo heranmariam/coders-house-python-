74d577b0701c970617b4973d503ed1313739bd27
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-12-09 11:43:06 +0530

    Create multi_FOR.py

---
+++ multi_FOR.py
@@ -0,0 +1,10 @@
+
+print("\nmultiplication table of 24")
+for i in range(1,11):
+    print(24,"*",i,"=",24*i)
+print("\nmultiplication table of 29")
+for i in range(1,11):
+    print(29,"*",i,"=",29*i)
+print("\nmultiplication table of 50")
+for i in range(1,11):
+    print(50,"*",i,"=",50*i)
