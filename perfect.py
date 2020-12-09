738e4bc11f30dd82f6de9069116534a1a9238555
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-12-09 11:54:52 +0530

    Update perfect.py

--- perfect.py
+++ perfect.py
@@ -3,11 +3,8 @@ perfect=0
 for i in range(1,n):
     if n%i==0:
         perfect=perfect+i
-        # print(i) venamekil print cheytha mathi
 if perfect==n:
     
     print("is a perfect num")
 else:
-    print("not a perfect num")        
-    
-# do perfect upto n
+    print("not a perfect num") 
