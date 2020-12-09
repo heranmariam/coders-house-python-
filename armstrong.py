dd5b114bce25221ec1b6aa65611ab11b9c04c84a
Heran Mariam Nechicattu <43461843+heranmariam@users.noreply.github.com>
2020-12-09 12:08:07 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+num=int(input("Enter any number"))
+k=num
+arm=0
+while num>0:
+    d=num%10
+    arm=(d**3)+arm
+    num=num//10
+if arm==k:
+    print("armstrong")
+else:
+    print("not armstrong")
